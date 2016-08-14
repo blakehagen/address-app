24734cf74fe1ec545c542462d35806da4a5fdb4f
Blake Hagen <blakehagen@gmail.com>
2016-08-13 21:56:19 -0600

    updating

--- www/js/features/user/userCtrl.js
+++ www/js/features/user/userCtrl.js
@@ -3,12 +3,16 @@ angular.module('addressApp').controller('userCtrl', function ($location, $stateP
   var userCtrl = this;
 
   userCtrl.loading = true;
+  userCtrl.edit = false;
 
   userCtrl.getUserData = function () {
     userService.getUserById($stateParams.id).then(function (response) {
       console.log('response', response);
       userCtrl.userData = response;
       userCtrl.loading = false;
+      if(!userCtrl.userData.Address.address1){
+        userCtrl.edit = true;
+      }
     })
   };
 
@@ -16,7 +20,12 @@ angular.module('addressApp').controller('userCtrl', function ($location, $stateP
 
   userCtrl.toggleMenu = function () {
     $location.path('/');
-  }
+  };
+
+  userCtrl.editMyAddress = function () {
+    userCtrl.edit = true;
+    console.log('edit clicked');
+  };
 
 
 
